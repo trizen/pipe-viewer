1c26c41d68e00d227ad2fffe2b6adb8dee169957
trizen <trizen@protonmail.com>
2021-02-10 01:17:04 +0200

    - Return the "LIVE" tag only for videos with "liveNow=true" "lengthSeconds=0".
    Past streams (that ended) now show the duration of the stream.

--- lib/WWW/PipeViewer/Utils.pm
+++ lib/WWW/PipeViewer/Utils.pm
@@ -717,7 +717,7 @@ sub get_duration {
 sub get_time {
     my ($self, $info) = @_;
 
-    if ($info->{liveNow}) {
+    if ($info->{liveNow} and ($self->get_duration($info) || 0) == 0) {
         return 'LIVE';
     }
 
