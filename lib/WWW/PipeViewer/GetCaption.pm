e7caa520f34a35edb5ed770a192abf47635a52dd
trizen <trizen@protonmail.com>
2021-03-02 14:43:29 +0200

    - Added the "--srt-languages=s" command-line option.
    It takes a list of comma-separated preferred language codes. It's particularily useful in combination with "--auto-captions" option to load auto-translated subtitles in a specific language, without editing the configuration file.
    
    Example:
    
    	# Romanian auto-translated subtitles
    	pipe-viewer --srt-languages=ro --auto-captions https://www.youtube.com/watch?v=iZnLZFRylbs

--- lib/WWW/PipeViewer/GetCaption.pm
+++ lib/WWW/PipeViewer/GetCaption.pm
@@ -206,17 +206,6 @@ sub xml2srt {
     return join("\n\n", @text);
 }
 
-=head2 get_xml_data($caption_data)
-
-Get the XML content for a given caption data.
-
-=cut
-
-sub get_xml_data {
-    my ($self, $url) = @_;
-    $self->{yv_obj}->lwp_get($url, simple => 1);
-}
-
 =head2 save_caption($video_ID)
 
 Save the caption in a .srt file and return its file path.
@@ -237,8 +226,9 @@ sub save_caption {
     return $srt_file if (-e $srt_file);
 
     # Get XML data, then transform it to SubRip data
-    my $xml = $self->get_xml_data($info->{baseUrl} // return) // return;
-    my $srt = $self->xml2srt($xml)                            // return;
+    my $url = $info->{baseUrl}                            // return;
+    my $xml = $self->{yv_obj}->lwp_get($url, simple => 1) // return;
+    my $srt = $self->xml2srt($xml)                        // return;
 
     # Write the SubRib data to the $srt_file
     open(my $fh, '>:utf8', $srt_file) or return;
