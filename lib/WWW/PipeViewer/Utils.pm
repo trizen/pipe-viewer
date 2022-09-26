63881dd0aa3348b67b8b22b57891babaf3c51356
Benoit Pierre <benoit.pierre@gmail.com>
2022-09-26 21:33:39 +0200

    minor cleanup

--- lib/WWW/PipeViewer/Utils.pm
+++ lib/WWW/PipeViewer/Utils.pm
@@ -812,25 +812,6 @@ sub get_channel_id {
 
 sub get_category_name {
     my ($self, $info) = @_;
-
-    state $categories = {
-                         1  => 'Film & Animation',
-                         2  => 'Autos & Vehicles',
-                         10 => 'Music',
-                         15 => 'Pets & Animals',
-                         17 => 'Sports',
-                         19 => 'Travel & Events',
-                         20 => 'Gaming',
-                         22 => 'People & Blogs',
-                         23 => 'Comedy',
-                         24 => 'Entertainment',
-                         25 => 'News & Politics',
-                         26 => 'Howto & Style',
-                         27 => 'Education',
-                         28 => 'Science & Technology',
-                         29 => 'Nonprofits & Activism',
-                        };
-
     $info->{genre} // $info->{category} // 'Unknown';
 }
 
